58b1963ecb63cb53e9252807859b8783e5b983ce
QuyenChoDien <nguyenlevanquyen08042004@gmail.com>
2023-10-24 16:04:18 +0700

    fix layout -> layouthome

---
+++ ecommerce-shop/Areas/Admin/Controllers/StaffController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ecommerce_shop.Areas.Admin.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Admin/Staff
+        public ActionResult DanhSach()
+        {
+            return View();
+        }
+        public ActionResult ThemMoi()
+        {
+            return View();
+        }
+        public ActionResult CapNhat()
+        {
+            return View();
+        }
+        public ActionResult Xoa()
+        {
+            return View();
+        }
+    }
+}
